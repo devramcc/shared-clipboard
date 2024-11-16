a578df848c72a69f929a53183c515334f71ebab2
Ramadhan <dev.ramddd@gmail.com>
2024-11-16 13:42:37 +0700

    [ProjectSetup] Starter Project

---
+++ backend/src/main/java/com/enigma/backend/SharedClipboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedClipboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedClipboardBackendApplication.class, args);
+	}
+
+}
